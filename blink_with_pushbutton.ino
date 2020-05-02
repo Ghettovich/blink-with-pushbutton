7612b4a97d30061177985c1e8beecdad95e96b7a
Ghettovich <mrfunkyman@pm.me>
2020-05-02 20:55:19 +0200

    init commit

---
+++ blink_with_pushbutton.ino
@@ -0,0 +1,58 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+
+const int buttonPin = 20, ledPin = 21, regularLedPin = 25;
+
+int buttonState = 0;
+void setup() {
+  
+  pinMode(ledPin, OUTPUT); 
+  
+  pinMode(regularLedPin, OUTPUT);
+  digitalWrite(regularLedPin, HIGH);
+  delay(1000);
+  digitalWrite(regularLedPin, LOW);
+  
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(regularLedPin, HIGH);
+    digitalWrite(ledPin, LOW);
+  } else {
+    // turn LED off:
+    digitalWrite(regularLedPin, LOW);
+    digitalWrite(ledPin, HIGH);
+  }
+}
