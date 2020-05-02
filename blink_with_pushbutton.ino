5b67722a092ae36efb6979fb2c83668c2ebc3245
Ghettovich <mrfunkyman@pm.me>
2020-05-02 23:54:25 +0200

    added servo control when pushing button

--- blink_with_pushbutton.ino
+++ blink_with_pushbutton.ino
@@ -1,7 +1,15 @@
+#include <Servo.h>
+
+int servoAngle = 0;
+const int servoPin = 27, interValMs = 50;
 const int buttonPin = 20, ledPin = 21, regularLedPin = 25;
 
 int buttonState = 0;
+
+Servo servo;
+
 void setup() {
+  servo.attach(servoPin);
   
   pinMode(ledPin, OUTPUT);   
   pinMode(regularLedPin, OUTPUT);
@@ -21,9 +29,25 @@ void loop() {
     // turn LED on:
     digitalWrite(regularLedPin, HIGH);
     digitalWrite(ledPin, LOW);
+    controlServo();
   } else {
     // turn LED off:
     digitalWrite(regularLedPin, LOW);
     digitalWrite(ledPin, HIGH);
   }
+  
+}
+
+void controlServo() {  
+  if (servoAngle >= 180) {    
+    for(;servoAngle = 0; servoAngle--) {
+        servo.write(servoAngle);
+        delay(10);      
+    }
+    servoAngle = 0;    
+  }
+  servoAngle++;
+  
+  servo.write(servoAngle);
+  delay(interValMs);
 }
