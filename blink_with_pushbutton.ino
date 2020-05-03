159a276e46a8f0050c86b7ccbed08715f8e452e2
Ghettovich <mrfunkyman@pm.me>
2020-05-03 20:26:25 +0200

    added another pushbutton and a sonar distance sensor (hc sr04). Added LED indicator when served reaches max position on either side.

--- blink_with_pushbutton.ino
+++ blink_with_pushbutton.ino
@@ -1,53 +1,132 @@
 #include <Servo.h>
 
-int servoAngle = 0;
-const int servoPin = 27, interValMs = 50;
-const int buttonPin = 20, ledPin = 21, regularLedPin = 25;
-
-int buttonState = 0;
-
 Servo servo;
+int servoAngle = 90;
+const int servoPin = 39, interValMs = 35;
 
+// Button clockwise
+const int buttonPinClockwise = 19, ledPinButtonClockwise = 29;
+
+// Button counter clockwise
+const int buttonPinCounterClockwise = 18, ledPinButtonCounterClockwise = 31;
+
+// indicator LEDs
+const int ledMaxRotation = 23, ledButtonPressed = 25;
+
+// Button states
+int buttonStateClockwise = 0, buttonStateCounterClock = 0;
+int lastButtonStateClockwise = LOW, lastButtonStateCounterClock = LOW;
+bool flipServoRotation = false;
+
+// HC SR04 sonar
+const int trigPin = 14, echoPin = 15;
+long duration;
+int distance;
+
+// Setup
 void setup() {
+  // Servo
   servo.attach(servoPin);
-  
-  pinMode(ledPin, OUTPUT);   
-  pinMode(regularLedPin, OUTPUT);
-  
+
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledMaxRotation, OUTPUT);
+  pinMode(ledButtonPressed, OUTPUT);
+  pinMode(ledPinButtonClockwise, OUTPUT);
+  pinMode(ledPinButtonCounterClockwise, OUTPUT);
+
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPinCounterClockwise, INPUT);
+  pinMode(buttonPinClockwise, INPUT);
+
+  // Define inputs and outputs for sonar
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  int reading = digitalRead(buttonPinClockwise);
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+  if (reading == HIGH) {
     // turn LED on:
-    digitalWrite(regularLedPin, HIGH);
-    digitalWrite(ledPin, LOW);
-    controlServo();
+    digitalWrite(ledButtonPressed, HIGH);
+    digitalWrite(ledPinButtonCounterClockwise, LOW);
+    digitalWrite(ledPinButtonClockwise, LOW);
+    turnServoClockwise();
   } else {
     // turn LED off:
-    digitalWrite(regularLedPin, LOW);
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledButtonPressed, LOW);
+    digitalWrite(ledPinButtonClockwise, HIGH);
   }
-  
+
+  reading = digitalRead(buttonPinCounterClockwise);
+
+  if (reading == HIGH) {
+    digitalWrite(ledButtonPressed, HIGH);
+    digitalWrite(ledPinButtonClockwise, LOW);
+    digitalWrite(ledPinButtonCounterClockwise, LOW);
+    turnServoCounterClockwise();
+  } else {
+    // turn LED off:
+    digitalWrite(ledButtonPressed, LOW);
+    digitalWrite(ledPinButtonCounterClockwise, HIGH);
+  }
+
+  printAndCalculateDistance();
+  toggleMaxRotationLED();
 }
 
-void controlServo() {  
-  if (servoAngle >= 180) {    
-    for(;servoAngle = 0; servoAngle--) {
-        servo.write(servoAngle);
-        delay(10);      
-    }
-    servoAngle = 0;    
+void turnServoClockwise() {
+  if (servoAngle < 180) {
+    servoAngle++;
+    servo.write(servoAngle);
+  } else {
+    servoAngle = 180;
   }
-  servoAngle++;
-  
-  servo.write(servoAngle);
+
   delay(interValMs);
+}
+
+void turnServoCounterClockwise() {
+  if (servoAngle >= 1) {
+    servoAngle--;
+    servo.write(servoAngle);
+  }
+  else {
+    servoAngle = 0;
+  }
+
+  delay(interValMs);
+}
+
+void toggleMaxRotationLED() {
+  if (servoAngle == 180 || servoAngle == 0) {
+    digitalWrite(ledMaxRotation, HIGH);
+  }
+  else {
+    digitalWrite(ledMaxRotation, LOW);
+  }
+}
+
+void printAndCalculateDistance() {
+  // Clear the trigPin by setting it LOW:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  // Trigger the sensor by setting the trigPin high for 10 microseconds:
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Read the echoPin, pulseIn() returns the duration (length of the pulse) in microseconds:
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance:
+  distance = duration * 0.034 / 2;
+  // Print the distance on the Serial Monitor (Ctrl+Shift+M):
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+  delay(50);
 }
